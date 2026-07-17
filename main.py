70aafe856bcb231c7dc086f89341fc9c0b2ba10f
archby <archby@protonmail.com>
2026-07-17 15:12:24 +0300

    changed button functions, added open button

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
@@ -9,23 +10,56 @@ root.title("Text Editor")
 root.geometry("1000x450")
 root.aspect(20, 9, 20, 9)
 
+current_file_path = None
+
+def update_window_title():
+    global current_file_path
+    if current_file_path:
+        file_name = os.path.basename(current_file_path)
+        root.title(f"Text Editor - {file_name}")
+    else:
+        root.title("Text Editor - New Document")
+
 def on_closing():
     if messagebox.askokcancel("Quit", "Do you want to quit? Make sure you saved!"):
         root.destroy()
-
 def on_save_button_click():
+    global current_file_path
     retrieved_text = text_area.get("1.0", "end-1c")
-    wsave = filedialog.asksaveasfilename(
+
+    if current_file_path is None:
+        selected_file = filedialog.asksaveasfilename(
+            defaultextension=".txt",
+            filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
+        )
+        if selected_file:
+            current_file_path = selected_file
+        else:
+            return
+    with open(current_file_path, "w") as file:
+        file.write(retrieved_text)
+    update_window_title()
+    messagebox.showinfo("Saved", "File saved successfully!")
+def on_new_button_click():
+    global current_file_path
+    if messagebox.askyesno("New File", "Are you sure you want to open a new document? Unsaved changes will be lost."):
+        text_area.delete("1.0", tk.END)
+        current_file_path = None
+        update_window_title()
+def on_open_button_click():
+    global current_file_path
+
+    selected_file = filedialog.askopenfilename(
         defaultextension=".txt",
         filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
     )
-    with open(wsave, "w") as file:
-        file.write(retrieved_text)
-def on_new_button_click():
-    if messagebox.askyesno("New File", "Are you sure you want to create a new file?"):
+
+    if selected_file:
+        current_file_path = selected_file
         text_area.delete("1.0", tk.END)
-    name = simpledialog.askstring("New File", "Name of the file? (creates a .txt file):")
-    f = open(f"{name}.txt", "x")
+        with open(current_file_path, "r") as file:
+            text_area.insert("1.0", file.read())
+        update_window_title()
 
 text_area = tk.Text(
     root,
@@ -44,7 +78,7 @@ save_button = tk.Button(
     root,
     text="Save",
     command=on_save_button_click,
-    bg="#007acc",
+    bg="#a3a3a3",
     fg="black"
 )
 save_button.pack(pady=5)
@@ -53,11 +87,20 @@ new_button = tk.Button(
     root,
     text="New",
     command=on_new_button_click,
-    bg="#007acc",
+    bg="#a3a3a3",
     fg="black"
 )
 
 new_button.pack(pady=5)
+
+open_button = tk.Button(
+    root,
+    text="Open",
+    command=on_open_button_click,
+    bg="#a3a3a3",
+    fg="black",
+)
+open_button.pack(pady=5)
 
 root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop()
